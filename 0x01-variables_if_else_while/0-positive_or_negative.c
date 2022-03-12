7e8729074f0423590b63387aca0f275ea67b34a4
Def-Rabiu <rabiukhalid2019@gmail.com>
2022-03-11 19:30:41 -0800

    simplr conditional statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
@@ -16,12 +17,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%i is positive\n", n);
-
-	else if (n < 0)
-		printf("%i is negative\n", n);
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is negative\n", n);
 	else
-		printf("%i is zero\n",  n);
+		printf("%d is zero\n",  n);
 
 	return (0);
 
