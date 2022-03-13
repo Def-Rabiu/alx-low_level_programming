ea07b8f0855f31b1467e81b952538b322030fb33
Def-Rabiu <rabiukhalid2019@gmail.com>
2022-03-13 04:09:36 -0700

    random num last digit with if else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,12 @@
-#include <stdio.h>                                                                                                                      
-#include <time.h>                                                                                                                       
-#include <stdlib.h>                                                                                                                     
-
-/**                                                                                                                                     
- * main - main block                                                                                                                    
- * Description: Get a random number and check its last digit, compare it with 5                                                         
- * Return: 0                                                                                                                            
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
 */
 int main(void)
-
 {
 	int n;
 	int last;
@@ -24,8 +22,6 @@ int main(void)
 	else if (last < 6)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 
-
 	return (0);
 
 }
-
