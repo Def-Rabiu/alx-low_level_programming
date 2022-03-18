88c63845d7898a4d836dd1f149c11590ce698fd5
Def-Rabiu <rabiukhalid2019@gmail.com>
2022-03-18 13:16:35 -0700

    checks for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+ * main - main func
+ * _isupper - checks if c is upper case
+ * @c: integer to check if it is upper
+ *
+ * Return: 1 if c uppercase else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90);
+		return (1);
+	return (0);
+}
